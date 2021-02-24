d0de8d54a1a156bc7d47fbfe2744bb3c2e5e5e5b
VickiYulianto10 <77873680+VickiYulianto10@users.noreply.github.com>
2021-02-24 15:20:45 +0700

    Add files via upload

---
+++ biodata_diri.java
@@ -0,0 +1,22 @@
+public class biodata_diri {
+   public static void main (String [] args) {
+       //deklarasi variable
+       String nama,alamat,hobi,sekolah,umur,tinggi_badan;
+
+       //pengisian variable
+       nama = "MOCH.VCIKI YULIANTO";
+       alamat = "LINGGO KEC.KEJAYAN KAB.PASURUAN";
+       hobi = "Playing Badminton";
+       sekolah = "SMKN 1 PURWOSARI";
+       umur = "17 Years Old";
+       tinggi_badan = "168 cm";
+
+       //tampil
+       System.out.println("NAMA : " + nama);
+       System.out.println("ALAMAT : " + alamat);
+       System.out.println("HOBI : " + hobi);
+       System.out.println("SEKOLAH : " + sekolah);
+       System.out.println("UMUR : " + umur);
+       System.out.println("TINGGI BADAN : " + tinggi_badan);
+   }
+}
